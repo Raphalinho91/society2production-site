90166249ddb5b8810db481c68a7752e7e1308178
Raphalinho91 <lokiju1598@outlook.fr>
2025-03-03 15:36:47 +0100

    Add Footer

---
+++ src/page/Footer.jsx
@@ -0,0 +1,40 @@
+import { Box, IconButton } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { PiTiktokLogo } from "react-icons/pi";
+
+export default function Footer() {
+  return (
+    <Box
+      id="footer"
+      sx={{
+        display: "flex",
+        width: "100%",
+        backgroundColor: "#121212",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 2,
+        pb: 2,
+        pt: 2,
+      }}
+    >
+      <a
+        href="https://www.instagram.com/2society.production/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconButton sx={{ color: "#FFF", transition: "all 0.3s ease" }}>
+          <InstagramIcon sx={{ fontSize: "35px" }} />
+        </IconButton>
+      </a>
+      <a
+        href="https://l.instagram.com/?u=https%3A%2F%2Fwww.tiktok.com%2F%402society.productions%3F_t%3DZN-8uBeOY6iAPW%26_r%3D1%26fbclid%3DPAZXh0bgNhZW0CMTEAAaauLAyiX3McMJlCz5LoW-ZHLsQHebKrHqXq5ymZexfJXXt5jDF6LY5M58M_aem_r8tXQXYSj-pxFm1NYQ6QFg&e=AT3FZrv_ZM0jX8ehJFn2vHru2_nWEo0t2bAjPqgZS24750y3149wovLY9rwWxhssfk6F24KmKS8chGi0psK_zvrnTQYDnHrVAnfLq8fX_ORoOOmYfiTMPA"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconButton sx={{ color: "#f15726", transition: "all 0.3s ease" }}>
+          <PiTiktokLogo style={{ fontSize: "35px" }} />
+        </IconButton>
+      </a>
+    </Box>
+  );
+}
